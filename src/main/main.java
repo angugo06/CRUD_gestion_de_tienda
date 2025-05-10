ed31ddfa6e0f6b62a6f3711d04d8a962f52f61ab
Angel Andre Guzman Gonzalez <87836480+angugo06@users.noreply.github.com>
2025-05-10 16:05:11 -0600

    Update main.java

--- src/main/main.java
+++ src/main/main.java
@@ -1,1 +1,6 @@
-
+public class Main {
+    public static void main(String[] args) {
+        InventarioDAO dao = new InventarioDAO();
+        dao.agregarArticulo("Papel", "Paquete de 500 hojas", 89.99, 10);
+    }
+}
